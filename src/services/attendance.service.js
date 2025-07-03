a3bff47a1aad0724d60cc6493b739b62e9485e3e
nimurr <nimurnerob404@gmail.com>
2025-07-03 16:41:41 +0600

    SMS is working perfectly

--- src/services/attendance.service.js
+++ src/services/attendance.service.js
@@ -198,8 +198,6 @@ const sendSMS = async (to, messageText) => {
 //     console.log("Test message sent to +447860004979");
 // })
 
-
-
 // Helper function to convert time string (e.g. "12:00 PM") to Date object
 const convertToDate = (timeStr) => {
     if (!timeStr) {
