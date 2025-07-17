b1c11c3924e6f9ff0f693f69c64ca66370e7e7c6
nimurr <nimurnerob404@gmail.com>
2025-07-17 11:43:43 +0600

    update

--- src/services/payment.service.js
+++ src/services/payment.service.js
@@ -22,13 +22,13 @@ const createPayment = async (body, userId) => {
     findUser.subscriptionEndDate = body.expiresDate; // consider setting to future date if needed
 
     await findUser.save();
-    const result = await Payment.create({ ...body, userId }); 
+    const result = await Payment.create({ ...body, userId });
 
     await Notification.create({
         userId: userId,
         content: `User payment has been successful ${body.amount} EUR for a ${body.duration} plan`,
         type: "success",
-    });
+    }); 
 
     return result;
 }
